fd3574e9283b99d277ae82268dabd71a3227d827
Wening Tyas Putri <dellarobbiaodd7@gmail.com>
2023-08-21 01:41:00 +0700

    first commit

---
+++ src/main/java/com/juaracoding/BankAccount.java
@@ -0,0 +1,47 @@
+package com.juaracoding;
+
+public class BankAccount {
+
+    int account_no;
+    String name;
+    double amount;
+
+    public BankAccount(int akun, String nama, double amount) {
+        this.account_no = akun;
+        this.name = nama;
+        this.amount = amount;
+    }
+
+    public String display() {
+        String display = account_no + " " + name + " " + amount;
+        return display;
+    }
+    public String checkBalance() {
+        String balance = "Balance is: " + amount;
+        return balance;
+    }
+
+    public double deposit(double amt) {
+        amount = amount + amt;
+        System.out.println(amt + " deposited");
+        return amount;
+    }
+
+    public double withdraw(double amt) {
+        if (amount < amt) { // 100k < 10k
+            System.out.println("Insufficient Balance");
+        } else {
+            amount = amount - amt;
+            System.out.println(amt + " withdrawn");
+        }
+        return amount;
+    }
+
+    public static void main(String[] args) {
+        BankAccount bank = new BankAccount(12345, "Tyas", 200_000);
+        bank.display();
+        bank.deposit(500_000);
+        bank.withdraw(100_000);
+        bank.checkBalance();
+    }
+}
---
+++ src/test/java/com/juaracoding/BankAccountTest.java
@@ -0,0 +1,22 @@
+package com.juaracoding;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class BankAccountTest {
+    BankAccount bank = new BankAccount(12345, "Tyas", 100_000.0);
+
+    @Test
+    public void testDeposit(){
+        double actual = bank.deposit(20_000);
+        double expect = 120_000;
+        Assert.assertEquals(actual, expect);
+    }
+    @Test
+    public void testWithdraw(){
+        double actual = bank.withdraw(10_000);
+        double expect = 110_000;
+        Assert.assertEquals(actual, expect);
+    }
+
+}
